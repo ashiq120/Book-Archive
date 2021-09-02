c4915d7bd1d2af203eb52b977d931524eb1fd3f4
Md. Ashiqur Rahman <62802119+ashiq120@users.noreply.github.com>
2021-09-02 21:05:37 +0600

    Done

---
+++ book.js
@@ -0,0 +1,88 @@
+search = () => {
+  const inputField = document.getElementById("inputField");
+  const bookCard = document.getElementById("bookCard");
+  const totalFound = document.getElementById("found");
+  const emptyInput = document.getElementById("emptyInput");
+  const error = document.getElementById("error");
+
+  const inputValue = inputField.value;
+  bookCard.textContent = "";
+  totalFound.innerText = "";
+  if (inputValue === "") {
+    sppiner("hidden");
+    emptyInput.style.display = "block";
+    error.style.display = "none";
+    totalFound.innerText = "";
+    bookCard.textContent = "";
+  } else {
+    sppiner("visible");
+    emptyInput.style.display = "none";
+    //Book url
+    const url = `http://openlibrary.org/search.json?q=${inputValue}`;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        displayBook(data);
+      });
+  }
+  inputField.value = "";
+};
+
+displayBook = (data) => {
+  const totalFound = document.getElementById("found");
+  totalFound.innerText = ` ${data.numFound} Result Founds `;
+
+  console.log("daattta", data);
+
+  const error = document.getElementById("error");
+  if (data.numFound === 0) {
+    totalFound.innerText = "";
+    error.style.display = "block";
+    sppiner("hidden");
+  } else {
+    error.style.display = "none";
+    const bookCard = document.getElementById("bookCard");
+
+    data?.docs.forEach((item) => {
+      const div = document.createElement("div");
+      console.log(item);
+      //show image
+      item?.cover_i
+        ? (imgUrl = `https://covers.openlibrary.org/b/id/${item?.cover_i}-M.jpg`)
+        : (imgUrl = "images/not found.jpg");
+
+      //author
+      item?.author_name ? (auth = item?.author_name.join()) : (auth = "not available");
+      //publisher
+      item?.publisher[0] ? (publisher = item?.publisher[0]) : (publisher = "not available");
+      //publish date
+      item?.publish_date[0] ? (publishDate = item?.publish_date[0]) : (publishDate = "not available");
+
+      console.log(item?.title);
+
+      div.innerHTML = `
+       <div class="col">
+           <div class="card">
+                <img height='450px'  src=${imgUrl}  class="card-img-top" alt="...">
+               <div class="card-body">
+                   <h5 id="author" class="card-title">${item?.title}</h5>
+                   <h6 class="card-text">Author:  <span class ="text-secondary"> ${auth} </span></h6>
+                   <h6 class="card-text">Publisher: <span class ="text-secondary"> ${publisher} </span> </h6>
+                   <h6 class="card-text">Published: <span class ="text-secondary">  ${publishDate} </span> </h6>
+
+               </div>
+           </div>
+       </div>
+       `;
+      bookCard.appendChild(div);
+      sppiner("hidden");
+    });
+  }
+};
+
+//Function
+sppiner = (property) => {
+  const sppiner = document.getElementById("sppiner");
+  sppiner.style.visibility = property;
+};
